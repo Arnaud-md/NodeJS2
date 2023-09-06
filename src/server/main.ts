7e8a301af3172cf0d61729edefae212f20a6f1cd
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-09-06 09:51:47 +0200

    first commit

---
+++ src/server/main.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import 'dotenv/config';
+
+const app = express();
+const port :number = parseInt(process.env.PORT as string)
+
+app.get('/hello/:nom/', (req, res) => {
+  const name = req.params.nom;
+  //console.log( parseInt(req.params.min));
+  
+  res.send("Hello "+name);
+})
+
+app.get("/hello", (_, res) => {
+  res.send("Hello Vite + TypeScript!");
+});
+
+ViteExpress.listen(app, port, () =>
+  console.log("Server is listening on port " + port + "...")
+);
